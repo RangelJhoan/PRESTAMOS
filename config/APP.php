de8298dcb83d6f00636c5c246373311147964cc3
RangelJhoan <jhoanrangels2@gmail.com>
2022-07-24 17:36:14 -0500

    Se agregó la configuración de la página y los controladores y modelos de configuración MVC

---
+++ config/APP.php
@@ -0,0 +1,11 @@
+<?php
+
+const SERVER_URL = "http://localhost/prestamos/";
+
+const COMPANY = "SISTEMAS PRESTAMOS";
+
+const MONEDA = "$";
+
+date_default_timezone_set("America/Bogota");
+
+?>
---
+++ controladores/vistasControlador.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "./modelos/vistasModelo.php";
+
+class vistasControlador extends vistasModelo{
+    /**
+     * Controlador para obtener plantilla
+     */
+    public function obtener_plantilla_controlador(){
+        return require_once "./vistas/plantilla.php";
+    }
+
+    /**
+     * Controlador para obtener las vistas
+     */
+    public function obtener_vistas_controlador(){
+        if(isset($_GET['views'])){
+            $ruta = explode("/", $_GET['views']);
+            $respuesta = vistasModelo::obtener_vistas_modelo($ruta[0]);
+        }else{
+            $ruta = "login";
+        }
+        return $respuesta;
+    }
+}
+
+?>
